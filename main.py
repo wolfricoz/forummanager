975961f58ef4aaed6493fb5ee866dbb5ce73ab63
wolfricoz <ricoisdenaam@gmail.com>
2025-01-27 20:21:00 +0100

    added join message and leave message

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from dotenv import load_dotenv
 import logging
 
 from classes.config import GuildConfig
+from classes.support.discord_tools import send_message
 
 # Creates database
 # loads env and variables
@@ -21,6 +22,7 @@ intents.members = True
 activity = discord.Activity(type=discord.ActivityType.watching, name="the forums")
 bot = commands.Bot(command_prefix=PREFIX, case_insensitive=False, intents=intents, activity=activity)
 bot.DEV = os.getenv("DEV")
+dev_channel = bot.get_channel(int(bot.DEV))
 
 
 # start up event; bot.tree.sync is required for the slash commands.
@@ -30,10 +32,19 @@ async def on_ready() :
 	guilds = [guild.name for guild in bot.guilds]
 	for guild in bot.guilds :
 		GuildConfig(guild.id)
-	dev_channel = bot.get_channel(int(bot.DEV))
 	await dev_channel.send(f"Bot started in {len(guilds)} guilds: {guilds}")
 	print("Commands synced, start up _done_")
 
+
+@bot.event
+async def on_guild_join(guild):
+	GuildConfig(guild.id)
+	await guild.system_channel.send("Hello! I am Forum Manager. I am here to help you with your forum channels. Please make sure to set up the channel permissions correctly and I will do the rest! The bot will automatically restore archived posts every 48 hours, if you want to trigger this manually, use the /forum recover command.")
+	await send_message(dev_channel, f"Bot joined {guild.name}({guild.id}) owned by {guild.owner.name}({guild.owner.id})")
+
+@bot.event
+async def on_guild_remove(guild):
+	await send_message(dev_channel, f"Bot removed from {guild.name}({guild.id}) owned by {guild.owner.name}({guild.owner.id})")
 
 # Grabs all the modules from the modules folder and loads them.
 @bot.event
